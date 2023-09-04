51a36021f3835f87d91cf37756e38144cb660829
Matee <matiullahsafibbc@gmail.com>
2023-09-05 02:36:04 +0430

    create navigation bar

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   resources :recipes
   resources :foods
   resources :users
+
+  get 'public_recipes', to: 'public_recipes#index'
+  get 'general_shopping_list', to: 'general_shopping_list#index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
