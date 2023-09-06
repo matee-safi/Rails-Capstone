5d539863e93ab2c9029e62087db831dbd105dcbd
Sumeya <sumeyaibrahim72@gmail.com>
2023-09-06 19:56:53 +0300

    Add recipe details

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :recipe_foods
-  resources :recipes
+  resources :recipes, only: [:index,:new,:create,:show,:destroy]
   resources :foods 
   resources :users
 
