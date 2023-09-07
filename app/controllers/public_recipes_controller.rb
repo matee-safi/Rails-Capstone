3c49fa8f77e170ff8b096dfd0d744451990425af
Matee <matiullahsafibbc@gmail.com>
2023-09-07 23:34:15 +0430

    create public recipes section

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,3 +1,6 @@
 class PublicRecipesController < ApplicationController
-  def index; end
+  def index
+    @public_recipes = Recipe.includes(:user).where(public: true)
+    @recipe_foods = RecipeFood.includes(:food).where(recipe_id: @public_recipes.map(&:id))
+  end
 end
