e1ff0e18e21988f83c836b5d1e3cee012383452c
Matee <matiullahsafibbc@gmail.com>
2023-09-09 15:10:01 +0430

    setup everything from scratch

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,7 +9,7 @@ class RecipesController < ApplicationController
   # GET /recipes/1 or /recipes/1.json
   def show
     @recipe = Recipe.find(params[:id])
-    @foods = Food.all
+    
   end
 
   # GET /recipes/new
